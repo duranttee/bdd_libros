857bcc3b6ebf8ca139d8f68e4ca6181f6f2b92ae
Tu nombre <L20350639@gmail.com>
2024-04-15 02:39:58 -0600

    actualización

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, Response, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import database as dbase  
 from product import Product
 
@@ -6,64 +6,64 @@ db = dbase.dbConnection()
 
 app = Flask(__name__)
 
-#Rutas de la aplicación
+# Rutas de la aplicación
 @app.route('/')
 def home():
     products = db['products']
     productsReceived = products.find()
-    return render_template('index.html', products = productsReceived)
+    return render_template('index.html', products=productsReceived)
 
-#Method Post
+# Método POST
 @app.route('/products', methods=['POST'])
 def addProduct():
     products = db['products']
     name = request.form['name']
     author = request.form['author']
-    
+    date = request.form['date']
+    product_id = request.form['id']
 
-    if name and author:
-        product = Product(name, author)
+    if name and author and product_id:
+        # Comprobar si el ID ya existe en la base de datos
+        existing_product = products.find_one({'id': product_id})
+        if existing_product:
+            return jsonify({'message': 'El ID ya está en uso.'}), 400
+
+        product = Product(product_id, name, author, date)
         products.insert_one(product.toDBCollection())
-        response = jsonify({
-            'name' : name,
-            'author' : author
-        })
         return redirect(url_for('home'))
     else:
         return notFound()
 
-#Method delete
-@app.route('/delete/<string:product_name>')
-def delete(product_name):
+# Método DELETE
+@app.route('/delete/<string:product_id>')
+def delete(product_id):
     products = db['products']
-    products.delete_one({'name' : product_name})
+    products.delete_one({'id': product_id})
     return redirect(url_for('home'))
 
-#Method Put
-@app.route('/edit/<string:product_name>', methods=['POST'])
-def edit(product_name):
+# Método PUT
+@app.route('/edit/<string:product_id>', methods=['POST'])
+def edit(product_id):
     products = db['products']
     name = request.form['name']
     author = request.form['author']
+    date = request.form['date']
 
     if name and author:
-        products.update_one({'name' : product_name}, {'$set' : {'name' : name, 'author' : author}})
-        response = jsonify({'message' : 'Producto ' + product_name + ' actualizado correctamente'})
+        products.update_one({'id': product_id}, {'$set': {'name': name, 'author': author, 'date': date}})
         return redirect(url_for('home'))
     else:
         return notFound()
 
 @app.errorhandler(404)
 def notFound(error=None):
-    message ={
+    message = {
         'message': 'No encontrado ' + request.url,
         'status': '404 Not Found'
     }
     response = jsonify(message)
     response.status_code = 404
     return response
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- product.py
+++ product.py
@@ -1,12 +1,17 @@
 class Product:
-    def __init__(self, name, author):
+    def __init__(self, id, name, author, date):
+        self.id = id
         self.name = name
         self.author = author
+        self.date = date
+        
         
 
     def toDBCollection(self):
         return{
+            'id' : self.id,
             'name': self.name,
-            'author': self.author
+            'author': self.author,
+            'date' : self.date
             
         }
